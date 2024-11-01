0aeea8f0fb9a5955ec1f217db8fb67f4ee86a74b
Jaejoon Han <jj2726@gmail.com>
2024-11-01 15:38:48 +0900

    add summary

--- Options/TestOptions.cs
+++ Options/TestOptions.cs
@@ -2,7 +2,18 @@
 
 public class TestOptions
 {
+    /// <summary>
+    /// this is string option
+    /// </summary>
     public string? StringOption { get; set; }
+
+    /// <summary>
+    /// this is int option
+    /// </summary>
     public int? IntOption { get; set; }
+
+    /// <summary>
+    /// this is boolean option
+    /// </summary>
     public bool? AnotherOption { get; set; }
 }
